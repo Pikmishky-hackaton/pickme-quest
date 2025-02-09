7a92318256c8dc6764e29efa0547119071e97275
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2025-02-10 01:48:10 +0200

    added serializer

---
+++ backend/users/serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password_confirm = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'date_of_birth', 'avatar', 'password']
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
