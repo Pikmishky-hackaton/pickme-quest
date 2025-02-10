af95c46a5f9bd9c8e09917c4cb81670d486b1367
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2025-02-10 23:47:55 +0200

    trying to add communication with backend during registration

--- frontend/src/app/components/header/Header.tsx
+++ frontend/src/app/components/header/Header.tsx
@@ -2,7 +2,7 @@ import ThemeSwitch from "@/components/theme/themeSwitch";
 
 export default function Header() {
   return (
-    <header className="bg-gray-100 dark:bg-gray-800 py-4 px-6 shadow-md">
+    <header className="bg-gray-100 dark:bg-gray-800 py-3 md:py-4 px-6 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-lg font-semibold text-emerald-700 dark:text-white">
           PickMe Quest
