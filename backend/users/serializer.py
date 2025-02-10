d739b2253279ec332b3e7af88b9cb90fa67f0995
Kiryao? <108414175+Kiryaoo@users.noreply.github.com>
2025-02-10 19:12:45 +0200

    update serializer.py

--- backend/users/serializer.py
+++ backend/users/serializer.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import User
 
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
     password_confirm = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'date_of_birth', 'avatar', 'password']
+        fields = ['username', 'first_name', 'last_name', 'email', 'date_of_birth', 'avatar', 'password', 'password_confirm']
 
     def validate(self, data):
         if data['password'] != data['password_confirm']:
@@ -16,10 +17,15 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password_confirm')
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+
+        user = User(**validated_data)
+        user.set_password(password)  # Хешуємо пароль
+        user.save()
         return user
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'avatar']
