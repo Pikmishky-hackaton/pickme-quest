682fe588855eca917ead589102bef35b0cb5ad12
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2025-02-10 23:47:55 +0200

    trying to add communication with backend during registration

--- frontend/src/components/google-login-button/GoogleLoginButton.tsx
+++ frontend/src/components/google-login-button/GoogleLoginButton.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 
 export default function GoogleLoginButton() {
   return (
-    <div className="w-full my-5 ">
+    <div className="w-full my-2 md:my-5 ">
       <button
-        className="w-full flex items-center justify-center px-4 py-2 border gap-2 border-slate-300 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
+        className="w-full flex items-center justify-center px-4 py-1 md:py-2 border gap-2 border-slate-300 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
         onClick={() => signIn("google")}
       >
         <Image
           width={5}
           height={5}
-          className="w-6 h-6"
+          className="w-3 h-3 md:w-6 md:h-3"
           src="https://www.svgrepo.com/show/475656/google-color.svg"
           loading="lazy"
           alt="google logo"
